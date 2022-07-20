040af67cbabc3f9bf5ac08a6d9fc12882cf12333
peng <pengx@alum.lehigh.edu>
2022-07-20 15:00:12 -0400

    update

--- assets/jsons/fake_server.php
+++ assets/jsons/fake_server.php
@@ -1,5 +1,6 @@
 <?php
 
+header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 
 $data = [
@@ -12,4 +13,3 @@ $data = [
 ];
 
 echo json_encode($data, JSON_PRETTY_PRINT);
-?>

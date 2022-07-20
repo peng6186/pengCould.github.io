8584a32b84274a7b2fc19c670ee079b6a992b917
peng <pengx@alum.lehigh.edu>
2022-07-20 14:46:24 -0400

    update

---
+++ assets/jsons/fake_server.php
@@ -0,0 +1,15 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+
+$data = [
+    [
+    "School" => "Lehigh University",
+    "Program" => "Financial Engineering",
+    "Type" => "MS",
+    "Year Conferred" => "2020"
+    ]
+];
+
+echo json_encode($data, JSON_PRETTY_PRINT);
+?>
